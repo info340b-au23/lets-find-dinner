acb9545387d025721dc1978cf462af20cac6e233
Richard Tran <RTran904@gmail.com>
2023-11-12 22:11:36 -0800

    Restructure App and added BankFinder.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import BankFinder from './BankFinder';
+
+function NavBar(props) {
+    return (
+        <h1>
+            Header
+        </h1>
+    )
+}
+
+export default function App(props) {
+    return (
+        <NavBar />
+    );
+}
