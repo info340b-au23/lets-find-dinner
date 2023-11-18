c5af01e88902dc8e822bd23dff0775597ba1a30c
Richard Tran <RTran904@gmail.com>
2023-11-18 15:48:44 -0800

    Finish hours regex for result display

--- src/components/bank-filter-components/ResultPanel.js
+++ src/components/bank-filter-components/ResultPanel.js
@@ -37,40 +37,66 @@ function ResultCard({bankData}) {
         setExpanded(!expanded);
     }
 
+    const fullAddress = bankData.address + ", " + bankData.city + ", WA, " + bankData.zip;
+    
+    const hours = Object.entries(bankData.hoursOpen).map(([day, time]) => {
+        let [timeStart, timeEnd] = time.split("-");
+        timeStart = timeStart.replaceAll(/([^:0-9])/g, "$1.");
+        timeStart = timeStart.replace(/(a|p)/g, " $1");
+        timeEnd = timeEnd.replaceAll(/([^:0-9])/g, "$1.");
+        timeEnd = timeEnd.replace(/(a|p)/g, " $1");
+        return <p><strong>{day}</strong>: {timeStart + " - " + timeEnd}</p>;
+    });
+
     return (
         <Col md="6" xl="4">
             <Card className="result-item my-2 shadow">
-                <Card.Header className="pt-4" aria-controls="test" aria-expanded={expanded} onClick={handleExpand}>
+                <Card.Header className="pt-4">
                     <Row>
                         <Col xs="1" className="d-flex align-items-center">
-                            {expanded ? <i class="fa-solid fa-angle-down icon-expand"></i> : <i class="fa-solid fa-angle-right icon-expand"></i>}
+                            <div aria-controls={bankData.bid + "-info"} aria-expanded={expanded} onClick={handleExpand}>
+                                {expanded ? 
+                                    <i className="fa-solid fa-angle-down icon-expand"></i> :
+                                    <i className="fa-solid fa-angle-right icon-expand"></i>
+                                }
+                            </div>
                         </Col>
                         <Col>
-                            <h3>Snohomish Community Food Bank</h3>
+                            <h3>{bankData.name}</h3>
                         </Col>
                     </Row>
                 </Card.Header>
                 <Collapse in={expanded}>
-                    <Card.Body id="test">
+                    <Card.Body id={bankData.bid + "-info"}>
                         <h4>Location</h4>
-                        <p>1330 Ferguson Park Rd, Snohomish, WA, 98292</p>
+                        <p>{fullAddress}</p>
                         <h4>Hours</h4>
-                        <p>Tuesday: 10am-2pm</p>
-                        <h4>Contact Info</h4>
+                        {hours}
+                        <h4>Contact</h4>
                         {bankData.phone &&
                             <div className="contact-info-group">
-                                <i class="fa-solid fa-phone"></i>
-                                <p>Phone</p>
+                                <a href={"tel:" + bankData.phone}>
+                                    <i className="fa-solid fa-phone"></i>
+                                    <p>Phone</p>
+                                </a>
                             </div>
                         }
-                        <div className="contact-info-group">
-                            <i class="fa-solid fa-envelope"></i>
-                            <p>Email</p>
-                        </div>
-                        <div className="contact-info-group">
-                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                            <p>Website</p>
-                        </div>
+                        {bankData.email &&
+                            <div className="contact-info-group">
+                                <a href={"mailto:" + bankData.email}>
+                                    <i className="fa-solid fa-envelope"></i>
+                                    <p>Email</p>
+                                </a>
+                            </div>
+                        }
+                        {bankData.website &&
+                            <div className="contact-info-group">
+                                <a href={bankData.website} target="_blank">
+                                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                                    <p>Website</p>
+                                </a>
+                            </div>
+                        }
                     </Card.Body>
                 </Collapse>
             </Card>
