017b02101f7f3f224fb5bcb37b921fbbe3e094f9
Richard Tran <RTran904@gmail.com>
2023-11-21 23:58:03 -0800

    Finish login page routing

--- src/components/App.js
+++ src/components/App.js
@@ -11,36 +11,35 @@ import { Routes, Route } from 'react-router-dom';
 import bankList from '../data/banks.json';
 
 export default function App(props) {
-    const [loggedIn, setLoginStatus] = useState(false);
-
-    const handleLogin = function() {
-        setLoginStatus(!loggedIn);
+    const [loggedUser, setLoginStatus] = useState(null);
+    
+    const handleLogin = function(user) {
+        setLoginStatus(user);
     };
+
+    const handleLogout = function() {
+        setLoginStatus(null);
+    }
 
     const uniqueCities = [...new Set(bankList.map((bankData) => {
         return bankData.city;
     }))].sort();
 
     // if user logs in after already logged in, send to account
-    // if user is logged in, clicking the sign out sends them to you are signed out.
-    // You are now signed out. Click here to return to login.
 
-    // login page: add form validation - see bootstrap react
     // signup-client - edit button link in login
     // signup-provider - edit button link in login
-    // logout
-    // 
 
     // later: check volunteer form, add header
     return (
         <div>
-            <NavBar logginIn={loggedIn} />
+            <NavBar user={loggedUser} handleLogout={handleLogout} />
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="volunteer" element={<Volunteer />} />
                 <Route path="find-a-food-bank" element={<BankFinder banks={bankList} cities={uniqueCities} />} />
-                <Route path="login" element={<Login />} />
+                <Route path="login" element={<Login loginCallback={handleLogin} />} />
                 <Route path="*" element={<MisDirect />} />
             </Routes>
         </div>
