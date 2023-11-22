246268cbb2d63ead0636622f3d7581b67cfe950a
soha sultana <sohasultana34@gmail.com>
2023-11-22 09:50:26 -0800

    updated contact with food bank

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,41 @@
 export default function ContactForm(props) {
  return (
-    <form>
+      <form>
         <h2 className="text-small">Fill out the form below.</h2>
         <div className="contact-information">
-          <section className="contact-field">
+          <section class="contact-field">
             <label for="name_input">Name</label>
-            <div className="name_input" type="name" name="name">
-             </div>
+            < input id ="name_input" type="name" name="name">
+              </input>
           </section>
           <section className="contact-field">
             <label for="email_input">Email</label>
-            <div className="email_input" type="email" name="email">
-            </div>
+            < input id ="email_input" type="email" name="email">
+              </input>
           </section>
           <section className="contact-field">
             <label for="age_input">Age</label>
-            <div className="age_input" type="age" name="age">
-                </div>
+            < input id ="age_input" type="age" name="age">
+              </input>
           </section>
           <section className="contact-field">
             <label for="phone_input">Phone</label>
-            <div className="phone_input" type="input" name="age">
-                </div>
+            < input id ="phone_input" type="input" name="age">
+              </input>
           </section>
           <section className="contact-field">
             <label for="zip_code_input">Zip Code</label>
-            <div className="zip_code_input" type="zip_code_input" name="zip_code_input">
-            </div>
+            < input id ="zip_code_input" type="zip_code_input" name="zip_code_input">
+              </input>
+          </section>
+          <section className="contact-field">
+            <label for="food_bank_input">Food Bank Name</label>
+            < input id ="food_bank_input" type="food_bank_input" name="food_bank_input">
+              </input>
           </section>
         </div>
         <button id="contact-submit-btn" className="rounded non-search-btn btn--darkred" type="button">Apply</button>
       </form>
  )
 }
-
-function checkValidity() {
-    
-}
-
 
