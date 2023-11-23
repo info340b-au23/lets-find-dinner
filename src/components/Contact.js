374c5ad9ca2a2f990f38a2f3b8901e9a1ddfbd8b
Richard Tran <RTran904@gmail.com>
2023-11-22 19:33:31 -0800

    Finish revisions to contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,125 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { useState } from 'react';
 
 export function VolunteerForm(props) {
+    const [validated, setValidated] = useState(null);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [age, setAge] = useState("");
+    const [zipCode, setZipCode] = useState("");
+    
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePhoneChange = (event) => {
+        setPhone(event.target.value);
+    }
+
+    const handleAgeChange = (event) => {
+        setAge(event.target.value);
+    }
+
+    const handleZipCodeChange = (event) => {
+        setZipCode(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        if (event.currentTarget.checkValidity() === false) {
+            setValidated(false);
+        } else {
+            setValidated(true);
+        }
+        event.preventDefault();
+    }
+
+    console.log(validated);
+
     return (
         <div>
-            <Container>
-
+            <Header title="Volunteer Form" background="volunteer-page" />
+            <Container className="text-content">
+                <Row>
+                    <h2 className="text-small">To apply for a volunteer position at a local food bank, fill out the application below.</h2>
+                    <Form id="volunteer-form" onSubmit={handleSubmit}>
+                        <Form.Group>
+                            <Form.Label htmlFor="name-input" className="volunteer-label">Name</Form.Label>
+                            <Form.Control
+                                id="name-input"
+                                className="volunteer-form-field"
+                                name="name-input"
+                                value={name}
+                                type="text"
+                                onChange={handleNameChange}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="email-input" className="volunteer-label">Email</Form.Label>
+                            <Form.Control
+                                id="email-input"
+                                className="volunteer-form-field"
+                                name="email-input"
+                                value={email}
+                                type="text"
+                                onChange={handleEmailChange}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="phone-input" className="volunteer-label">Phone</Form.Label>
+                            <Form.Control
+                                id="phone-input"
+                                className="volunteer-form-field"
+                                name="phone-input"
+                                value={phone}
+                                type="text"
+                                onChange={handlePhoneChange}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="age-input" className="volunteer-label">Age</Form.Label>
+                            <Form.Control
+                                id="age-input"
+                                className="volunteer-form-field"
+                                name="age-input"
+                                value={age}
+                                type="text"
+                                onChange={handleAgeChange}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="zip-code-input" className="volunteer-label">Zip Code</Form.Label>
+                            <Form.Control
+                                id="zip-code-input"
+                                className="volunteer-form-field"
+                                name="zip-code-input"
+                                value={zipCode}
+                                type="text"
+                                onChange={handleZipCodeChange}
+                            />
+                        </Form.Group>
+                        {validated && <p>You have successfully applied!</p>}
+                        <Button
+                            id="volunteer-submit-btn"
+                            variant="danger"
+                            className="non-search-btn btn--dark-red"
+                            type="submit"
+                        >
+                            Apply
+                        </Button>
+                    </Form>
+                </Row>
             </Container>
-        <form>
-            <h2 className="text-small">Fill out the form below.</h2>
-            <div className="contact-information">
-            <section className="contact-field">
-                <label for="name_input">Name</label>
-                <div className="name_input" type="name" name="name">
-                </div>
-            </section>
-            <section className="contact-field">
-                <label for="email_input">Email</label>
-                <div className="email_input" type="email" name="email">
-                </div>
-            </section>
-            <section className="contact-field">
-                <label for="age_input">Age</label>
-                <div className="age_input" type="age" name="age">
-                    </div>
-            </section>
-            <section className="contact-field">
-                <label for="phone_input">Phone</label>
-                <div className="phone_input" type="input" name="age">
-                    </div>
-            </section>
-            <section className="contact-field">
-                <label for="zip_code_input">Zip Code</label>
-                <div className="zip_code_input" type="zip_code_input" name="zip_code_input">
-                </div>
-            </section>
-            </div>
-            <button id="contact-submit-btn" className="rounded non-search-btn btn--darkred" type="button">Apply</button>
-        </form>
+            <Footer fixFooter={false} />
         </div>
     )
 }
