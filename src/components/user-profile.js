008737d55fe858fc470efd87f425fb51c186cf33
carolyan929 <carolyan@uw.edu>
2023-12-04 00:57:52 -0800

    update user-profile structure

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -15,54 +15,62 @@ export default function UserProfile(props) {
 }
 
 function ReturnUserData(userName){
+    const filteredUser = userList.filter(data => data.username === userName);
+
+    const userTable = filteredUser.map(item => {
+        let shifts = item.volunteerHistory;
+        const volunteerHistoryTable = Object.keys(shifts).map((key, data) => {
+            return (
+                <tr>
+                    <td>{key}:</td>
+                        <td>{
+                            <ul>{Object.keys(data).map((key, data) => {
+                                return (
+                                    <li>{key}: {data}</li>
+                                )
+                            })}</ul>
+                        }</td>
+                </tr>
+            );
+        });
+        return (
+            <table key={item.username}>
+                <tbody>
+                    <tr>
+                        <td>Username:</td>
+                        <td>{item.username}</td>
+                    </tr>
+                    <tr>
+                        <td>Name:</td>
+                        <td>{item.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>{item.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>{item.phone}</td>
+                    </tr>
+                    <tr>
+                        <td>Volunteer History:</td>
+                        <td>
+                            <table>
+                                <tbody>{volunteerHistoryTable}</tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    });
+
     return (
         <div>
-        <h2>Profile Information</h2>
-
-        <section className="profile-content">
-            {userList.filter(data => data.username === userName).map(item => {
-                let shifts = item.volunteerHistory;
-                return (
-                    <table>
-                        <tr>
-                            <td>Username:</td>
-                            <td>{item.username}</td>
-                        </tr>
-                        <tr>
-                            <td>Name:</td>
-                            <td>{item.name}</td>
-                        </tr>
-                        <tr>
-                            <td>Email:</td>
-                            <td>{item.email}</td>
-                        </tr>
-                        <tr>
-                            <td>Phone:</td>
-                            <td>{item.phone}</td>
-                        </tr>
-                        <tr>
-                            <td>Volunteer History:</td>
-                            <td>{
-                                <table>{Object.keys(shifts).map((key, data) => {
-                                    return (
-                                        <tr>
-                                            <td>{key}:</td>
-                                            <td>{
-                                                <ul>{Object.keys(data).map((key, data) => {
-                                                    return (
-                                                        <li>{key}: {data}</li>
-                                                    )
-                                                })}</ul>
-                                            }</td>
-                                        </tr>
-                                    )
-                                })}</table>
-                            }</td>
-                        </tr>
-                    </table>
-                )
-            })}
-        </section>
+            <h2>Profile Information</h2>
+            <section className="profile-content">
+                {userTable}
+            </section>
         </div>
-    )
+    );
 }
