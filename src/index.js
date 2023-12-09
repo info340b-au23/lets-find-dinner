b806a5c43d4aed1bea4627b29bc7d8f406e5a1d3
Richard Tran <rtran904@gmail.com>
2023-12-08 20:11:40 -0800

    add firebase config to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSjUgcZJgVaXAnFLkQgvv8CBMLreO6yCU",
+  authDomain: "lets-find-dinner.firebaseapp.com",
+  projectId: "lets-find-dinner",
+  storageBucket: "lets-find-dinner.appspot.com",
+  messagingSenderId: "257731440186",
+  appId: "1:257731440186:web:42aa4c3756372abf648f03"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
